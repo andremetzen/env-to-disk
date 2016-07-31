3e0c65b5fa1e555703428ddc47425db68335f604
Andre Metzen <andressantos@gmail.com>
2016-07-31 15:24:53 -0300

    Support to recursive directory creating

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 "use strict"
 
-let fs = require('fs');
+let fs = require('fs'),
+	mkpath = require('mkpath');
 
 
 if(!process.env.VARIABLE_LIST) {
@@ -19,11 +20,18 @@ envs.forEach((name) => {
 		return;
 	}
 
-	var [path, encoding, data] = process.env[name].split(separator);
-	console.log(path, encoding, data);
+	let [path, encoding, data] = process.env[name].split(separator);
 
-	if(path && encoding && data)
-		fs.writeFile(path, new Buffer(data, encoding));
+	if(path && encoding && data) {
+		let dir = path.substring(0,path.lastIndexOf("/")+1);
+		mkpath(dir, function (err) {
+			if (err) throw err;
+			fs.writeFile( path, new Buffer( data, encoding ), function(err, done) {
+				if (err) throw err;
+				console.log('Succefully written: >>>\n', new Buffer( data, encoding ).toString(), "\n<<<\nto "+path)
+			} );
+		});
+	}
 	else
 		console.error("Missing params\n", "path", path, "\n", "encoding", encoding, "\n", "data", data );
 });
