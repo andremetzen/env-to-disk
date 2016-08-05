471bc6359e588e23fb5075ebb341ff64faa96a42
Josmar Dias <josmarnet@gmail.com>
2016-08-05 14:42:43 -0300

    beautify without changing what seems to be the code standard

--- index.js
+++ index.js
@@ -3,9 +3,8 @@
 let fs = require('fs'),
 	mkpath = require('mkpath');
 
-
-if(!process.env.VARIABLE_LIST) {
-	console.error( 'Couldn\'t find environment variable "VARIABLE_LIST".' );
+if (!process.env.VARIABLE_LIST) {
+	console.error('Couldn\'t find environment variable "VARIABLE_LIST".');
 	process.end(-1);
 }
 
@@ -14,24 +13,22 @@ let separator = process.env.SEPARATOR || "|";
 
 envs.forEach((name) => {
 	name = name.toUpperCase();
-	if(!process.env[name])
-	{
-		console.error('Couldn\'t find environment variable "'+name+'".');
+	if (!process.env[name]) {
+		console.error('Couldn\'t find environment variable "' + name + '".');
 		return;
 	}
 
 	let [path, encoding, data] = process.env[name].split(separator);
 
-	if(path && encoding && data) {
-		let dir = path.substring(0,path.lastIndexOf("/")+1);
-		mkpath(dir, function (err) {
+	if (path && encoding && data) {
+		let dir = path.substring(0, path.lastIndexOf("/") + 1);
+		mkpath(dir, function(err) {
 			if (err) throw err;
-			fs.writeFile( path, new Buffer( data, encoding ), function(err, done) {
+			fs.writeFile(path, new Buffer(data, encoding), function(err, done) {
 				if (err) throw err;
-				console.log('Succefully written: >>>\n', new Buffer( data, encoding ).toString(), "\n<<<\nto "+path)
-			} );
+				console.log('Succefully written: >>>\n', new Buffer(data, encoding).toString(), "\n<<<\nto " + path)
+			});
 		});
-	}
-	else
-		console.error("Missing params\n", "path", path, "\n", "encoding", encoding, "\n", "data", data );
+	} else
+		console.error("Missing params\n", "path", path, "\n", "encoding", encoding, "\n", "data", data);
 });
